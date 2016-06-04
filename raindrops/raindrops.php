884390160a1e8f81c839d28cf9b65e7f38fcde46
Manuel Gómez <mgdepoo@gmail.com>
2016-06-05 00:59:59 +0200

    Fourth kata: raindrops

---
+++ raindrops/raindrops.php
@@ -0,0 +1,15 @@
+<?php
+
+function raindrops($num)
+{
+    $raindrops = [3 => 'Pling', 5 => 'Plang', 7 => 'Plong'];
+    $response = '';
+
+    foreach ($raindrops as $factor => $raindrop) {
+        if ($num % $factor == 0) {
+            $response .= $raindrop;
+        }
+    }
+
+    return $response ?: (string) $num;
+}
