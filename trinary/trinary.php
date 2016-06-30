2e401c8af81f95f302ba801c57404f4d1d5d9e6a
Manuel Gómez <mgdepoo@gmail.com>
2016-06-30 14:15:07 +0200

    Trinary refactor

--- trinary/trinary.php
+++ trinary/trinary.php
@@ -3,16 +3,14 @@
 function toDecimal($trinary)
 {
 	$result = 0;
-	$digits = str_split($trinary);
-	$reversed = array_reverse($digits);
+	$reversed = array_reverse(str_split($trinary));
 
 	foreach ($reversed as $position => $trinary) {
-		if (is_nan($trinary) or $trinary > 2 or $trinary < 0) {
+		if (is_nan($trinary) || $trinary > 2 || $trinary < 0) {
 			return 0;
 		}
 
-		$orderOfMagnitude = $position == 0 ? 1 : pow(3, $position);
-		$result += $orderOfMagnitude * $trinary;
+		$result += pow(3, $position) * $trinary;
 	}
 
 	return $result;
